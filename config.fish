c7897c4cc9cd9a56a9827de3942af8ed87f604c7
kobari12 <kobari0124@gmail.com>
2023-01-14 13:59:36 +0900

    Update: add conda init

--- config.fish
+++ config.fish
@@ -1,10 +1,10 @@
-#view
+##view
 set -g theme_display_date yes
 set -g theme_date_format "+%F %H:%M"
 set -g theme_display_git_default_branch yes
 set -g theme_color_scheme dark
 
-# alias
+## alias
 alias vim=nvim
 alias vi=vim
 alias rm=trash-put
@@ -15,7 +15,7 @@ alias la='ls -aA'
 set LESSHISTFILE /dev/null
 set EDITOR vim
 
-# fzf
+## fzf
 set FZF_LEGACY_KEYBINDINGS 0
 set FZF_DEFAULT_COMMAND 'fd -t f -I -H -E ".git" -E ".cache" . ~'
 set FZF_DEFAULT_OPTS '--preview "bat -nf {}" --reverse --border --height 40%'
@@ -23,11 +23,11 @@ set FZF_FIND_FILE_COMMAND $FZF_DEFAULT_COMMAND
 set FZF_OPEN_COMMAND $FZF_DEFAULT_COMMAND
 set FZF_CD_COMMAND $FZF_DEFAULT_COMMAND
 
-# conda
-set -U fish_user_paths "/opt/miniconda3/bin" $fish_user_paths
+## conda
+set PATH /opt/miniconda3/bin $PATH
 source (conda info --root)/etc/fish/conf.d/conda.fish
 
-# yay & fisher list
+## yay & fisher list
 yay -Qeq    > ~/.dotfile/yay.list
 fisher list > ~/.dotfile/fisher.list
 
